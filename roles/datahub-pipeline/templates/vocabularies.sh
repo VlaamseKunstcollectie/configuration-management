6d51f3053d8f61eb22750b49fcf41b34e0fd9c24
vkc-001 <vkc-001@kitania.be>
2021-03-22 15:45:17 +0000

    Adds datahub, pipeline and dashboard WIP

---
+++ roles/datahub-pipeline/templates/vocabularies.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+FILES=('PIDS_ALL_UTF8' 'RIGHTS' 'CREATORS_UTF8' 'AAT_UTF8' 'CA_UTF8')
+
+for file in "${FILES[@]}"; do
+  echo "Generating $file.";
+  catmandu import CSV to DBI --data_source "dbi:SQLite:/tmp/import.${file}.sqlite" < "{{ datahub_pipeline['authority_files']['dir'] }}/${file}.csv"
+done
+
