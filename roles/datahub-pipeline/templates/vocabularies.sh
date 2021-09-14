74e6c3f7329c1aee0a02e3425a6ff799925743a1
vkc-001 <vkc-001@kitania.be>
2021-09-14 13:25:36 +0000

    Finalizes and enables letsencrypt, adds solr service

--- roles/datahub-pipeline/templates/vocabularies.sh
+++ roles/datahub-pipeline/templates/vocabularies.sh
@@ -4,5 +4,5 @@ FILES=('PIDS_ALL_UTF8' 'RIGHTS' 'CREATORS_UTF8' 'AAT_UTF8' 'CA_UTF8')
 
 for file in "${FILES[@]}"; do
   echo "Generating $file.";
-  PERL5LIB=/home/{{ application_user }}/perl5/lib/perl5/ /home/{{ application_user }}/perl5/bin/catmandu import CSV to DBI --data_source "dbi:SQLite:/tmp/import.${file}.sqlite" < "{{ datahub_pipeline.dir }}/authority_files/${file}.csv"
+  PERL5LIB=~{{ application_user }}/perl5/lib/perl5/ ~{{ application_user }}/perl5/bin/catmandu import CSV to DBI --data_source "dbi:SQLite:/tmp/import.${file}.sqlite" < "{{ datahub_pipeline.dir }}/authority_files/${file}.csv"
 done
